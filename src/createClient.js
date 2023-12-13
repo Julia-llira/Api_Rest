4b4c4b6ad424d4b63c3a50dc5c16607d02f44d22
Julia-llira <144963279+Julia-llira@users.noreply.github.com>
2023-12-13 04:51:31 +0000

    atualizações feitas

--- src/createClient.js
+++ src/createClient.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 
 export const supabase= createClient(
-    "https://tupmvvksqgbqdzridrqb.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1cG12dmtzcWdicWR6cmlkcnFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjcwMTIyNTcsImV4cCI6MTk4MjU4ODI1N30.5uZdCofPa878ZbK6jbrl6pJBLmn11XC9BAausn2tD2U"
+    "https://dbagfrolazhukzrvxfsv.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRiYWdmcm9sYXpodWt6cnZ4ZnN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI0NDE1NDcsImV4cCI6MjAxODAxNzU0N30.h8rN5kVIFCEi9LXlcFm5cyk8SLorCbWy_esVR2Q5DuY"
     )
